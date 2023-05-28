ab0c82788d7129dcfdd6534525a0c0f4a6e0bbad
Shewone2004 <134802138+Shewone2004@users.noreply.github.com>
2023-05-28 21:37:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['14246441438', 'Jasmine ✨', true],
+  ['14246441438', 'Jasmine ✨', true], 
   [''] 
 ] //Number of owners
 
@@ -47,12 +47,12 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'SANDARU-YT'
+global.botname = 'ɪ ᴀᴍ ɢᴜʀᴜ ʙᴏᴛ 🫨'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
+global.packname = 'GURU┃ᴮᴼᵀ ✨' 
 global.author = '@Asliguru' 
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
+global.dygp = 'https://chat.whatsapp.com/LCqVgrFjYom1gIGFtZEDXw'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT-V1' 
 global.fgyt = 'https://youtube.com/@Asliguru'
 global.fgpyp = 'https://youtube.com/@Asliguru'
